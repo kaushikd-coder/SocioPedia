cd176454599d3c7f71b3a38f17b6f96b4a43ff8b
Kaushik das <kaushik1641.cse19@chitkara.edu.in>
2023-01-08 13:45:07 +0530

    Sc03

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -7,7 +7,7 @@ const UserImage = ({ image, size = "60px" }) => {
                 style={{ objectFit: 'cover', borderRadius:'50%'}}
                 width={size}
                 height={size}
-                src={`http://localhost:3001/assets/${image}`}
+                src={`https://sociopedia-7bdx.onrender.com/assets/${image}`}
                 alt="img"
             />
         </Box>
